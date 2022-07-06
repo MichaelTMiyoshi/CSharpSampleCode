d27ec54e7739f22036a078b27e5fda81fce7074d
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-06 07:42:47 -0700

    07/06/2022  Hello World!  Plus a couple other lines.  Used Console.Write() as well (which does not end with a newline).
    Note:
    I do not like that the .cs file defaults to Program.cs.  I like to be given the chance to name the file rather than having to manually rename it.

---
+++ CSharpSamplesProject/CSharpSamplesProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpSamplesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello\n\nWorld!");
+            Console.WriteLine();
+        }
+    }
+}
