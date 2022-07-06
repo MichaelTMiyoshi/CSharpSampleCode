b49cc4a680452ee8ef7eb4b192e3b81578528075
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-06 08:31:14 -0700

    07/06/2022 Created project for identifiers (variables and constants).
    Added comments to both projects (hello world and identifiers).
    Had to tell Visual Studio that the new project was for .NET 5.0 and then change its target afterward so that it had all the namespace, class, and Main stuff.
    [Author Note: Writing example code while writing the book is a good process.]

--- CSharpSamplesProject/CSharpSamplesProject/Program.cs
+++ CSharpSamplesProject/CSharpSamplesProject/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*	Name:  (Put your name here)
+	Problem:  (Put the whole problem statement here, or use your own words.)
+	Pseudocode:  (State your algorithm in English here.)
+	Notes:  (Put any notes about your thoughts here)
+	Maintenance log:
+        Date:		Done:
+        (Date here)	(What you did here)
+*/
+using System;
 
 namespace CSharpSamplesProject
 {
---
+++ CSharpSamplesProject/FirstIdentifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿/*	Name:  Michael T. Miyoshi
+	Problem:  Declare and initialize variables.
+	Pseudocode:  
+        Output message to the screen
+        Declare and initialize variable and constant
+	Notes:  Copy Git commit message to maintenance log.
+	Maintenance log:
+        Date:		Done:
+        07/06/2022	Sample code created (variables and constants)
+*/
+using System;
+
+namespace FirstIdentifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  // output to the screen
+            int someVariable = 0;
+            const double someConstant = 10.0;
+            int someChar = 'q';
+        }
+    }
+}
