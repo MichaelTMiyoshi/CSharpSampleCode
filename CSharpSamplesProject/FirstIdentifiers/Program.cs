f8dd5389ce4a196cc1bf64d1131d66f7af04c398
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-07 08:31:23 -0700

    07/07/2022 Added code to change the variables (and the constant).  Also added a bool variable and output it.  Am leaving the line of code that will break it in the code so that people see the error.

--- CSharpSamplesProject/FirstIdentifiers/Program.cs
+++ CSharpSamplesProject/FirstIdentifiers/Program.cs
@@ -1,12 +1,16 @@
-﻿/*	Name:  Michael T. Miyoshi
-	Problem:  Declare and initialize variables.
-	Pseudocode:  
+﻿/*	Name:  (Put your name here)
+	Problem:  (Put the whole problem statement here, or use your own words.)
+        Declare and initialize variables.
+	Pseudocode:  (State your algorithm in English here.)
         Output message to the screen
         Declare and initialize variable and constant
-	Notes:  Copy Git commit message to maintenance log.
-	Maintenance log:
+	Notes:  (Put any notes about your thoughts here.)
+        Copy Git commit message to maintenance log.
+	Maintenance log:    (You can copy your detailed Git commit messages.)
         Date:		Done:
+        (Date here) (What you did here)
         07/06/2022	Sample code created (variables and constants)
+        07/07/2022  Output variables.  Added formatting and a bool.
 */
 // This is a comment
 using System;
@@ -21,11 +25,24 @@ namespace FirstIdentifiers
             int someVariable = 0;
             const double someConstant = 10.0;
             char someChar = 'q';
-            string someString = "Woo hoo!";
+            string someString = "Goodbye...";
+            bool someBool = true;
             Console.WriteLine("someVariable = " + someVariable);
             Console.WriteLine("someConstant = " + someConstant);
             Console.WriteLine("someChar = " + someChar);
             Console.WriteLine("someString = " + someString);
+            Console.WriteLine(String.Format("someConstant (3 decimal places) = {0:0.000}", someConstant));
+
+            // some additional code
+            someVariable = 14;
+            someConstant = 15;  // this will cause an error
+            someChar = 'x';
+            someString = "for now.";
+            Console.WriteLine("someVariable = " + someVariable);
+            Console.WriteLine(String.Format("someConstant = {0:0.000}", someConstant));
+            Console.WriteLine("someChar = " + someChar);
+            Console.WriteLine("someString = " + someString);
+            Console.WriteLine("someBool = " + someBool);
         }
     }
 }
