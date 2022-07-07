ffb045966de56ee744640318f3515fb1df482707
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-07 14:30:24 -0700

    07/07/2022 String concatenation.  Finished examples.  Reminded again that strings are immutable in C# (and java).

---
+++ CSharpSamplesProject/StringConcatenation/StringConcatenation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringConcatenation
+{
+    internal class StringConcatenation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s;
+            s = "";
+            Console.WriteLine(s);
+            s = "Something";
+            Console.WriteLine(s);
+            s = s + " else";
+            Console.WriteLine(s);
+            s += " goes here";
+            Console.WriteLine(s);
+
+            string t, u;
+            s = "Something";
+            t = "else";
+            u = "where";
+            s = s + " " + t;
+            Console.WriteLine(s);
+            s += " goes here";
+            Console.WriteLine(s);
+            s = "";
+            s = u + " " + t;
+            Console.WriteLine(s);   // what is the output?
+            s = "???" + s;
+            Console.WriteLine(s);   // what is the output?
+        }
+    }
+}
