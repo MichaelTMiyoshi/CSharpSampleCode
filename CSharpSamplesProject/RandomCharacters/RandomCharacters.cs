444e58e397e72caddea8117e67eb70f84fdd5ff7
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-08 09:56:59 -0700

    07/08/2022 Random numbers.  Both int and float. Created Random character project too.  Just printed out all the chars at first.

---
+++ CSharpSamplesProject/RandomCharacters/RandomCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomCharacters
+{
+    internal class RandomCharacters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 128; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+    }
+}
---
+++ CSharpSamplesProject/RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    internal class RandomNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Numbers");
+            Random rng = new Random();
+            int rn = rng.Next();
+            Console.WriteLine(rn);
+            rn = rng.Next(10);
+            Console.WriteLine(rn);
+            int min = 1, max = 6;
+            Console.WriteLine("Die roll [1-6]");
+            rn = min + rng.Next(max - min + 1);
+            Console.WriteLine(rn);
+            Console.WriteLine("Not a die roll [1-6)");
+            rn = min + rng.Next(max - min);
+            Console.WriteLine(rn);
+            Console.WriteLine("Random [7-37)");
+            min = 7;
+            max = 37;
+            rn = rng.Next(min, max);
+            Console.WriteLine(rn);
+            Console.WriteLine("Random [7-17]");
+            min = 7;
+            max = 17;
+            rn = rng.Next(min, max + 1);
+            Console.WriteLine(rn);
+            Console.WriteLine("Random double [0.0, 1.0)");
+            double rd = rng.NextDouble();
+            Console.WriteLine(rd);
+            Console.WriteLine("Random double [0.0, 6.8)");
+            double dmax = 6.8;
+            rd = rng.NextDouble() * dmax;
+            Console.WriteLine(rd);
+        }
+    }
+}
