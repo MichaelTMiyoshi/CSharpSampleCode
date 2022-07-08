c13b8e8ff02075074a9ba32901535077cfbcd075
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-08 10:43:26 -0700

    07/08/2022 Random chars.  Two ways to do it.

--- CSharpSamplesProject/RandomCharacters/RandomCharacters.cs
+++ CSharpSamplesProject/RandomCharacters/RandomCharacters.cs
@@ -6,11 +6,16 @@ namespace RandomCharacters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            for(int i = 0; i < 128; i++)
-            {
-                Console.WriteLine((char)i);
-            }
+            Console.WriteLine("Random Characters");
+            Random rng = new Random();
+            int min = 32;
+            int max = 127;
+            int rn = rng.Next(min, max);
+            Console.WriteLine((char)rn);
+            char cmin = ' ';
+            char cmax = '~';
+            char rc = (char)rng.Next(cmin, cmax + 1); ;
+            Console.WriteLine(rc);
         }
     }
 }
