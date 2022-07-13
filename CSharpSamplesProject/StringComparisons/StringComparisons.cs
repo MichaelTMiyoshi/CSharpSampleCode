83871a52d92556b9a7e7a6f68e89274541332006
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-13 10:01:35 -0700

    07/13/2022 Finished looping examples.  The code is a bit messy all together, but split up in the book, it looks good.
    Did some string comparisons in the StringComparisons project.  Just put in code for ignoring case in strings when checking for equailty and order.

---
+++ CSharpSamplesProject/StringComparisons/StringComparisons.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StringComparisons
+{
+    internal class StringComparisons
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string Comparisons");
+
+            string s1 = "Alpha";
+            string s2 = "alpha";
+            string s3 = "omega";
+
+            Console.WriteLine("s1: " + s1);
+            Console.WriteLine("s2: " + s2);
+            Console.WriteLine("s3: " + s3);
+
+            if (s1.Equals(s2))
+            {
+                Console.WriteLine("The strings s1 and s2 are equal.");
+            }
+            else if(s1.Equals(s2, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("The strings " + s1 + " and " + s2 + " are equal, ignoring the case of letters.");
+            }
+            else
+            {
+                Console.WriteLine("The strings " + s1 + " and " + s2 + " are not equal.");
+            }
+
+            if(0 < String.Compare(s1, s3, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(s3 + " goes before " + s1 + " alphabetically.");
+            }
+            else if(String.Compare(s1, s3, StringComparison.OrdinalIgnoreCase) < 0)
+            {
+                Console.WriteLine(s1 + " goes before " + s3 + " alphabetically.");
+            }
+            else
+            {
+                Console.WriteLine("The strings " + s1 + " and " + s3 + " are equal.");
+            }
+        }
+    }
+}
