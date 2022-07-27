b70fad938637587e6824f6f05bcc2ca304403172
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-07-27 12:18:46 -0700

    07/27/2022 Get console data one char at a time with Console.ReadKey().  (StringBuilder is strange when getting a char at a time using Console.ReadKey().)

---
+++ CSharpSamplesProject/Functions/Functions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Functions
+{
+    internal class Functions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpSamplesProject/NotEatingNewlines/NotEatingNewlines.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;  // for StringBuilder
+
+namespace NotEatingNewlines
+{
+    internal class NotEatingNewlines
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Not Eating Newlines");
+            Console.WriteLine("Input a string (it can have tabs, " +
+                "spaces, and newlines).  Press the ESCAPE key to end.");
+            String test = "";
+            //StringBuilder test = new StringBuilder();
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey();
+                if (key.Key != ConsoleKey.Escape)
+                {
+                    if(key.Key == ConsoleKey.Enter)
+                    {
+                        test += "\n";
+                        //test.Append("\n");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        test += Convert.ToChar(key.KeyChar);
+                        //test.Append(Convert.ToChar(key.Key));
+                    }
+                }
+                
+            } while (key.Key != ConsoleKey.Escape);
+            
+            Console.WriteLine("n"); // this is because the ESC key is active
+            Console.WriteLine("This is the output");
+            Console.WriteLine(test);
+            //String s = test.ToString();
+            //Console.WriteLine(s);
+        }
+    }
+}
