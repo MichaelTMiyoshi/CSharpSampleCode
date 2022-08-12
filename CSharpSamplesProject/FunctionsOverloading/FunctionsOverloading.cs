5c8bd6984e04302d6c0f45909c64d7a6471d8229
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-12 12:13:52 -0700

    08/12/2022 Function overloading and passing data by reference.

---
+++ CSharpSamplesProject/FunctionsOverloading/FunctionsOverloading.cs
@@ -0,0 +1,29 @@
+﻿namespace FunctionsOverloading
+{
+    internal class FunctionsOverloading
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Overloading functions");
+            String example = "Hello";
+            Console.WriteLine(Function(example));
+            Console.WriteLine(Function(example, 0));
+            Console.WriteLine(Function(example, 7));
+        }
+        static String Function(String someString)
+        {
+            return someString + " world.";
+        }
+        static String Function(String someString, int num)
+        {
+            if(num == 0)
+            {
+                return someString + " universe.";
+            }
+            else
+            {
+                return someString + " there.";
+            }
+        }
+    }
+}
--- CSharpSamplesProject/FunctionsSimplePassData/FunctionsSimplePassData.cs
+++ CSharpSamplesProject/FunctionsSimplePassData/FunctionsSimplePassData.cs
@@ -8,12 +8,22 @@
             int a = 4, b = 5;
             int ans = SomeFunction(a, ref b);
             Console.WriteLine("The answer of SomeFunction(" + a + "," + b + ") is: " + ans);
+            String s = "Hello ";
+            String t = "World ";
+            String u = SomeFunction(s, ref t);
+            Console.WriteLine("The answer of SomeFunction(" + s + "," + t + ") is: " + u);
         }
         static int SomeFunction(int a, ref int b)
         {
             b = b * b;
             a += b;
             return a / b;
+        }
+        static String SomeFunction(String a, ref String b)
+        {
+            a += b;
+            b = a + b;
+            return a;
         }
     }
 }
