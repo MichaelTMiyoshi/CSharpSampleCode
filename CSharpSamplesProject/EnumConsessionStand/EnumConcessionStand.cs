a1b2bed111b3f36d5c642a493743d04cf6aee325
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-13 08:51:45 -0700

    08/13/2022 enum examples.  Need to add one more example still.

---
+++ CSharpSamplesProject/EnumConsessionStand/EnumConcessionStand.cs
@@ -0,0 +1,44 @@
+﻿namespace EnumConcessionStand
+{
+    internal class EnumConcessionStand
+    {
+        enum Sizes { None, Small, Medium, Large, Jumbo }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Concession Stand");
+            Sizes drink = Sizes.None, popcorn = Sizes.None;
+            double total = 0;
+
+            Console.WriteLine("Welcome to the concession stand.");
+            Console.WriteLine("We have drinks and popcorn available.");
+            Console.WriteLine("Drinks and popcorn come in Small, Medium, Large, and Jumbo sizes.");
+            Console.WriteLine("Choose your drink size.");
+            Console.WriteLine("\t0. None\n\t1. Small $1.50\n\t2. Medium $2.50\n\t3. Large $4.00\n\t4. Jumbo $6.00");
+            drink = (Sizes)Convert.ToInt32(Console.ReadLine());
+            Console.Write(">> ");
+            switch(drink)
+            {
+                case Sizes.None:
+                    Console.WriteLine("You have chosen not to get a drink.");
+                    break;
+                case Sizes.Small:
+                    total += 1.5;
+                    break;
+                case Sizes.Medium:
+                    total += 2.5;
+                    break;
+                case Sizes.Large:
+                    total += 4.0;
+                    break;
+                case Sizes.Jumbo:
+                    total += 6.0;
+                    break;
+                default:
+                    Console.WriteLine("Not a choice");
+                    break;
+            }
+
+            Console.WriteLine("Total: {0,10:C2}", total);
+        }
+    }
+}
---
+++ CSharpSamplesProject/EnumExample/EnumExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace EnumExample
+{
+    internal class EnumExample
+    {
+        enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+        enum Months { January = 1, February, March, April, May, June, 
+                      July, August, September, October, November, December }
+        enum Mem { Bit = 1, Nibble = 4, Byte = 8 }
+        enum Amounts { Each = 1, Dozen = 12, Gross = 144 }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enum Example");
+            Days day = Days.Sunday;
+            Console.WriteLine(day + " = " + (int)day);
+            Months month = Months.September;
+            Console.WriteLine(month + " = " + (int)month);
+            Mem memory = Mem.Nibble;
+            Console.WriteLine(memory + " = " + (int)memory);
+            Amounts amount = Amounts.Gross;
+            Console.WriteLine(amount + " = " + (int)amount);
+        }
+    }
+}
