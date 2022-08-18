b46ef83a912795dd75eafd9493a92c6518e40564
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-18 14:56:23 -0700

    08/18/2022 2D List.

--- CSharpSamplesProject/ListStringExample/ListStringExample.cs
+++ CSharpSamplesProject/ListStringExample/ListStringExample.cs
@@ -13,7 +13,49 @@
             Console.WriteLine(fortunes[1][11]); // the output is a 'z'
 
             Console.WriteLine("2D List");
-            List<List<int>> int3x4 = new List<List<int>>();
+            List<List<int>> int3x4 = new List<List<int>> { new List<int> { 0, 1, 2, 3},
+                                                           new List<int> {4, 5, 6, 7 },
+                                                           new List<int> {8, 9, 10, 11 }};
+
+            for (int r = 0; r < int3x4.Count; r++)
+            {
+                for(int c = 0; c < int3x4[r].Count; c++)
+                {
+                    Console.Write("{0,5}", int3x4[r][c]);
+                }
+                Console.WriteLine();
+            }
+
+            int3x4.Clear();
+            Console.WriteLine("Empty List\n\n");
+            for (int r = 0; r < int3x4.Count; r++)
+            {
+                for (int c = 0; c < int3x4[r].Count; c++)
+                {
+                    Console.Write("{0,5}", int3x4[r][c]);
+                }
+                Console.WriteLine();
+            }
+
+            for(int r = 0; r < 5; r++)
+            {
+                List<int> inner = new List<int>();
+                for (int c = 0; c < 6; c++)
+                {
+                    inner.Add(r * c);
+                }
+                int3x4.Add(inner);
+            }
+            Console.WriteLine("newly created 2D List (5x6)");
+            for (int r = 0; r < int3x4.Count; r++)
+            {
+                for (int c = 0; c < int3x4[r].Count; c++)
+                {
+                    Console.Write("{0,5}", int3x4[r][c]);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
