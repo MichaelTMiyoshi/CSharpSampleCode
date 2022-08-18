9e73d872775a20db5e67df2a08d6751a8e8b153c
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-18 09:59:35 -0700

    08/18/2022 Lists.
    Instantiating, initializing.  Adding, removing, changing are next.

---
+++ CSharpSamplesProject/ListExample/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeTester
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle() // default constructor
+        {
+            Radius = 0;
+        }
+
+        public Circle(double Radius)    // constructor with argument(s)
+        {
+            this.Radius = Radius;
+        }
+
+        public Circle(Circle C) // copy constructor
+        {
+            Radius = C.Radius;
+        }
+
+        public double Circumference()
+        {
+            return Math.PI * Radius * 2.0;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override String ToString()
+        {
+            String output = String.Format("Radius:    {0, 8:0.000}\n", Radius);
+            output += String.Format("Perimeter: {0, 8:0.000}\n", Circumference());
+            output += String.Format("Area:      {0, 8:0.000}\n", this.Area());
+            return output;
+        }
+    }
+}
---
+++ CSharpSamplesProject/ListExample/ListExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using ShapeTester;
+namespace ListExample
+{
+    internal class ListExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List Example");
+            String fortune = "What is the any key?";
+            List<int> scores = new List<int>();
+            scores.Add(1);
+            scores.Add(2);
+            for(int i = 3; i <= 10; i++)
+            {
+                scores.Add(i);
+            }
+
+            Console.WriteLine(fortune);
+            Console.WriteLine(scores);
+            List<double> doubles = new List<double>();
+            List<bool> TFAnswers = new List<bool>();
+            List<Circle> circles = new List<Circle>();
+
+            List<int> nums = new List<int> {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            foreach (int i in nums)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine();
+            List<double> health = new List<double> { 10.5, 9.6, 8.7, 7.6, 6.5 };
+            Console.WriteLine(health[3]);
+            health[3] = 16.5;
+            Console.WriteLine(health[3]);
+        }
+    }
+}
