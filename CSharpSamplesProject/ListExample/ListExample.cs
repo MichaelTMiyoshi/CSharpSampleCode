fd9d60b67640d9c9784e3718e748ded4c48e286f
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-18 14:25:44 -0700

    08/18/2022 Lists almost done.
    Need to complete 2D lists.

--- CSharpSamplesProject/ListExample/ListExample.cs
+++ CSharpSamplesProject/ListExample/ListExample.cs
@@ -33,6 +33,67 @@ namespace ListExample
             Console.WriteLine(health[3]);
             health[3] = 16.5;
             Console.WriteLine(health[3]);
+
+            Console.WriteLine("int List");
+            List<int> intList = new List<int>();
+            for(int i = 0; i < 100; i++)
+            {
+                intList.Add(i + 1);
+            }
+
+            OutputList(intList);
+
+            intList.Remove(10);     // removes the number 10
+            intList.Remove(20);     // removes the number 20
+            intList.RemoveAt(33);   // removes the number at location 33 (36)
+            intList.RemoveAt(33);   // (37)
+            intList.Insert(39, 101);
+            intList[6] = 17;
+            intList[9] = intList[9] * 10;
+
+            Console.WriteLine("int List with removals and insertion and changes");
+            OutputList(intList);
+            Console.WriteLine();
+
+            Console.WriteLine("double List");
+            List<double> doubleList = new List<double>();
+            for(int i = 0; i < intList.Count; i++)
+            {
+                doubleList.Add(intList[i] * 1.5);
+            }
+
+            OutputList(doubleList);
+        }
+
+        static void OutputList(List<int> list)
+        {
+            int counter = 0;
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0,8}", list[i] + "\t");
+                counter++;
+                if (counter == 10)  // puts ten items on a line
+                {
+                    Console.WriteLine();
+                    counter = 0;
+                }
+            }
+        }
+        static void OutputList(List<double> list)
+        {
+            int counter = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0,7:0.00}", list[i] + "\t");
+                //Console.Write("{0,7:0.00}\t", list[i]);
+                //Console.Write(String.Format("{0,10:0.000}", list[i]));
+                counter++;
+                if (counter == 10)
+                {
+                    Console.WriteLine();
+                    counter = 0;
+                }
+            }
         }
     }
 }
---
+++ CSharpSamplesProject/ListStringExample/ListStringExample.cs
@@ -0,0 +1,19 @@
+﻿namespace ListStringExample
+{
+    internal class ListStringExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of Strings");
+            List<String> fortunes = new List<String> { "You will have a great day",
+                "You are amazing!", 
+                "What is the any key?" };
+
+            Console.WriteLine(fortunes[2]);
+            Console.WriteLine(fortunes[1][11]); // the output is a 'z'
+
+            Console.WriteLine("2D List");
+            List<List<int>> int3x4 = new List<List<int>>();
+        }
+    }
+}
