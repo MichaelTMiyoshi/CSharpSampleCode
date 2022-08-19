61be7b1083c0b81b6109831319128a5014731a7f
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-08-19 11:19:53 -0700

    08/19/2022 FileReadingExample
    Read from the stream and output to the screen.

---
+++ CSharpSamplesProject/FileReadingExample/FileReadingExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FileReadingExample
+{
+    internal class FileReadingExample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading Files");
+            String folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            StreamReader reader = new StreamReader($"{folder}" +
+                $"{Path.DirectorySeparatorChar}" +
+                $"Documents{Path.DirectorySeparatorChar}InputTextFile.txt");
+
+            // the file is located in the directory:
+            // Documents/Documents  (on a Windows machine)
+            // Documents            (on a Mac)
+
+            while (!reader.EndOfStream)
+            {
+                String line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
