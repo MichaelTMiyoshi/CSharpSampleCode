ca486cbb1accf67ef1ef084948e1224ea2b79e5b
Michael T. Miyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2022-12-08 17:48:53 -0800

    12/08/2022 Fraction reducer and square root reducer added as example function programs.

---
+++ CSharpSamplesProject/FunctionFractionReducer/FunctionFractionReducer.cs
@@ -0,0 +1,31 @@
+﻿namespace FunctionFractionReducer
+{
+    internal class FunctionFractionReducer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fraction Reducer");
+            Console.Write("Input numerator: ");
+            int numerator = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input denominator: ");
+            int denominator = Convert.ToInt32(Console.ReadLine());
+
+            int factor = GCF(numerator, denominator);   // function call
+            Console.WriteLine("the Greatest Common Factor is: " + factor);
+        }
+
+        static int GCF(int num1, int num2)  // function signature
+        {
+            // below is the function definition
+            int gcf = 1;
+            for(int i = 2; i <= num1; i++)
+            {
+                if(num1 % i == 0 && num2 % i == 0)
+                {
+                    gcf = i;
+                }
+            }
+            return gcf; // must match the return type
+        }
+    }
+}
---
+++ CSharpSamplesProject/FunctionRootReducer/FunctionRootReducer.cs
@@ -0,0 +1,29 @@
+﻿namespace FunctionRootReducer
+{
+    internal class FunctionRootReducer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Square Root Reducer");
+            Console.Write("Input a number to find the reduced square root: √");
+            int number = Convert.ToInt32(Console.ReadLine());
+            (int outside, int inside) = RootReducer(number);
+            Console.WriteLine("√" + number + " = " + outside + "√" + inside);
+        }
+
+        static (int, int) RootReducer(int n)
+        {
+            int outside = 1;
+            int inside = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % (i * i) == 0)
+                {
+                    outside = i;
+                    inside = n / (i * i);
+                }
+            }
+            return (outside, inside);
+        }
+    }
+}
